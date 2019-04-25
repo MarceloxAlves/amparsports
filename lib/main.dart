7668220da798a578fd4c5eebefc89c86bf522ad5
Marcelo Alves <marceloalvessoft@gmail.com>
2019-04-25 12:40:47 -0300

    AmparSport init

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/homepage.dart';
+import 'package:flutter_firebase/loginpage.dart';
+import 'package:flutter_firebase/signup.dart';
+void main()=>runApp(FireAuth());
+
+class FireAuth extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      title: "Firebase Auth",
+      debugShowCheckedModeBanner: false,
+theme: ThemeData(primarySwatch: Colors.blue),
+      home:LoginPage(),
+      routes: <String,WidgetBuilder>{
+        "/userpage":(BuildContext context)=>new Page(),
+        "/loginpage":(BuildContext context)=>new LoginPage(),
+        "/signup":(BuildContext context)=>new SignUpPage()
+
+      },
+    );
+  }
+}
